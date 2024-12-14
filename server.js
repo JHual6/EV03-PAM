7d33f168d2b538b8ba0ff3f0dab37478009e1c58
JHual6 <jo.huala@duocuc.cl>
2024-12-14 17:07:02 -0300

    encabezados

--- server.js
+++ server.js
@@ -10,8 +10,17 @@ const PORT = process.env.PORT || 4000;
 
 
 // Middleware
-app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; img-src 'self' data:;"
+  );
+  next();
+});
 
 // Configuración de la base de datos
 const db = mysql.createPool({
