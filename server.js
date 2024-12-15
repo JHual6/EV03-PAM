ee366fa4b3ef76ba3a4fbe98d3df4c5a16b7f2bb
JHual6 <jo.huala@duocuc.cl>
2024-12-15 14:34:31 -0300

    Cambios en servidor

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
