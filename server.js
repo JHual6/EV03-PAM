ffd804a0a67c8626edbae40082074c1f3db137f9
JHual6 <jo.huala@duocuc.cl>
2024-12-14 23:39:12 -0300

    cambios finales 2.0 para ver las clases 6

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const moment = require('moment');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
